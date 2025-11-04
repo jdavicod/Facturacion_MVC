93d274c5c7471645601192e8c88d4b9eaac1cf25
Juan David Garcia Vargas <gvargasjd@gmail.com>
2025-11-03 21:05:20 -0500

    Agregar archivos de proyecto.

---
+++ Facturacion_MVC/Controllers/PrincipalController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Facturacion_MVC.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ayuda()
+        {
+            return View();
+        }
+
+        public IActionResult Acerca()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+    }
+}
